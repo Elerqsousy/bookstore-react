38e070d77db761bca756e92c2b0bcedad18da88f
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-26 11:58:29 +0200

    Fix linters errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Book = (props) => (
-  <div>
-    <span>{props.category}</span>
-    <h2>{props.title}</h2>
-    <span>{props.author}</span>
-  </div>
-);
+const Book = (props) => {
+  const { category, title, author } = props;
+  return (
+    <div>
+      <span>{category}</span>
+      <h2>{title}</h2>
+      <span>{author}</span>
+    </div>
+
+  );
+};
+
+Book.propTypes = {
+  category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
 
 export default Book;
--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Book from './Book';
 
-const BookCard = (props) => (
-  <div>
-    <Book
-      category={props.category}
-      title={props.title}
-      author={props.author}
-    />
-    <button type="button" onClick={null}>
-      Remove
-    </button>
-  </div>
-);
+const BookCard = (props) => {
+  const { category, title, author } = props;
+  return (
+    <div>
+      <Book
+        category={category}
+        title={title}
+        author={author}
+      />
+      <button type="button" onClick={null}>
+        Remove
+      </button>
+    </div>
+  );
+};
+
+BookCard.propTypes = {
+  category: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
 
 export default BookCard;

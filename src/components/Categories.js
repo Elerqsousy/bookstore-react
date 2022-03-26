19b1f7828416e965880d3539e951ffcbd4a8804c
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-26 11:21:22 +0200

    Add categories page component

---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => {
+  return (
+    <div>
+      <button type='button' onClick={null}>Check status</button>
+    </div>
+  )
+}
+
+export default Categories;

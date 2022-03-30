d67b7ae2e424422c45810dbce1198d112352c747
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-03-30 14:36:46 +0200

    Combine all reducers using combineReducers function and use it to create store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { combineReducers, createStore } from 'redux';
+import bookReducer from './books/books';
+import categoryReducer from './categories/categories';
+
+const rootReducer = combineReducers({
+  books: bookReducer,
+  categories: categoryReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
